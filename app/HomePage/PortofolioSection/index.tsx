a2b5139a2f84938e07b1025101a4a6b1d8a7f2ad
johannes debritto <johanesdebrito22@gmail.com>
2025-01-17 23:30:56 +0700

    Fix

--- app/HomePage/PortofolioSection/index.tsx
+++ app/HomePage/PortofolioSection/index.tsx
@@ -1,13 +1,12 @@
 import Portofolio from "./Portofolio";
 import Testimoni from "./Testimoni";
-import ImageCarousel from "./ImageCarousel";
+
 
 const PortofolioSection = () => {
     return (
         <div>
        <Portofolio />
        <Testimoni />
-       {/* <ImageCarousel/> */}
        
           
 
