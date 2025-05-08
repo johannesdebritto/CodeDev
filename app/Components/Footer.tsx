b87a4fddfbf5026cc45b82bdbc0fc42dacad905f
johannes debritto <johanesdebrito22@gmail.com>
2025-05-08 11:02:04 +0700

    push

--- app/Components/Footer.tsx
+++ app/Components/Footer.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Image from 'next/image';
+import Image from "next/image";
 const Footer = () => {
   return (
     <footer className="bg-black text-white py-6">
@@ -7,21 +7,12 @@ const Footer = () => {
         {/* Left Section - Logo and Description */}
         <div className="col-span-1">
           <div className="flex items-center space-x-2">
-          <Image
-            src="/images/logo/logo.svg"
-            alt="CodeDev Logo"
-            width={40}
-            height={40}
-            className="object-contain"
-          />
+            <Image src="/images/logo/logo.svg" alt="CodeDev Logo" width={40} height={40} className="object-contain" />
             <h3 className="text-lg font-semibold">
               Code<span className="text-blue-600">Dev</span>
             </h3>
           </div>
-          <p className="text-sm mt-2">
-            Wujudkan Solusi Digital Terbaik Bersama CodeDev untuk Bisnis yang
-            Lebih Modern, Efisien, dan Berkembang.
-          </p>
+          <p className="text-sm mt-2">Wujudkan Solusi Digital Terbaik Bersama CodeDev untuk Bisnis yang Lebih Modern, Efisien, dan Berkembang.</p>
           <p className="text-sm mt-4 text-gray-400">&copy; 2024 All Rights Reserved</p>
         </div>
 
@@ -30,43 +21,19 @@ const Footer = () => {
           <h4 className="font-semibold mb-4">Follow us</h4>
           <div className="flex space-x-2">
             <div className="relative w-6 h-6">
-            <Image
-              src="/images/icon/logo_twitterwhite.svg"
-              alt="Twitter"
-              width={40}
-              height={40}
-              className="object-contain transition-transform duration-300 transform hover:scale-110"
-            />
+              <Image src="/images/icon/logo_twitterwhite.svg" alt="Twitter" width={40} height={40} className="object-contain transition-transform duration-300 transform hover:scale-110" />
             </div>
             <div className="relative w-6 h-6">
-            <Image
-              src="/images/icon/logo_tiktokwhite.svg"
-              alt="TikTok"
-              width={40}
-              height={40}
-              className="object-contain transition-transform duration-300 transform hover:scale-110"
-            />
+              <Image src="/images/icon/logo_tiktokwhite.svg" alt="TikTok" width={40} height={40} className="object-contain transition-transform duration-300 transform hover:scale-110" />
             </div>
             <div className="relative w-6 h-6">
-            <Image
-              src="/images/icon/logo_igwhite.svg"
-              alt="Instagram"
-              width={40}
-              height={40}
-              className="object-contain transition-transform duration-300 transform hover:scale-110"
-            />
+              <Image src="/images/icon/logo_igwhite.svg" alt="Instagram" width={40} height={40} className="object-contain transition-transform duration-300 transform hover:scale-110" />
             </div>
           </div>
           <h4 className="font-semibold mt-2 mb-2">Call us</h4>
           <div className="flex items-center space-x-2">
             <div className="relative w-6 h-6">
-            <Image
-              src="/images/icon/logo_wawhite.svg"
-              alt="WhatsApp"
-              width={40}
-              height={40}
-              className="object-contain"
-            />
+              <Image src="/images/icon/logo_wawhite.svg" alt="WhatsApp" width={40} height={40} className="object-contain" />
             </div>
             <p className="text-sm">+1 600 654 36-60</p>
           </div>
@@ -86,15 +53,8 @@ const Footer = () => {
                 Android Developers
               </a>
             </li>
-            <li>
-              <a href="/LayananUXUIPage" className="hover:underline">
-                UX/UI Designer
-              </a>
-            </li>
           </ul>
         </div>
-
-
       </div>
     </footer>
   );
--- app/HomePage/LayananSection/layanan.tsx
+++ app/HomePage/LayananSection/layanan.tsx
@@ -92,36 +92,6 @@ const Layanan = () => {
               </a>
             </div>
           </article>
-
-          {/* Jasa UX/UI */}
-          <article className="grid grid-cols-1 md:grid-cols-2 gap-4 items-center">
-            <figure className="flex flex-col items-center">
-              <Image src="/images/icondekstop/ux.svg" alt="Ilustrasi layanan desain UX/UI" width={400} height={400} className="object-contain" priority />
-            </figure>
-            <div className="mt-8">
-              <div className="flex space-x-4 items-center mb-2">
-                <Image src="/images/icon/logo_figma.svg" alt="Logo Figma" width={40} height={40} />
-              </div>
-              <h3 className="text-[22px] font-bold text-blue-600 mb-2">Jasa Pembuatan Desain UX / UI</h3>
-              <hr className="h-0.5 bg-black mb-2 border-0" />
-              <p className="text-gray-700 text-justify font-semibold text-[19px]">
-                Transformasikan ide Anda menjadi desain UX/UI yang menarik dan fungsional. Kami menciptakan solusi desain yang fokus pada pengalaman pengguna, memastikan tampilan modern dan interaksi yang intuitif untuk mendukung kesuksesan
-                bisnis Anda.
-              </p>
-              <a
-                href="/LayananUXUIPage"
-                className="relative inline-flex items-center justify-center p-4 px-6 py-1 overflow-hidden font-medium text-indigo-600 transition duration-300 ease-out border-2 border-purple-500 rounded-full shadow-md group mt-2"
-              >
-                <span className="absolute inset-0 flex items-center justify-center w-full h-full text-white duration-300 -translate-x-full bg-purple-500 group-hover:translate-x-0 ease">
-                  <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M14 5l7 7m0 0l-7 7m7-7H3" />
-                  </svg>
-                </span>
-                <span className="absolute flex items-center justify-center w-full h-full text-purple-500 transition-all duration-300 transform group-hover:translate-x-full ease">Lihat Detail</span>
-                <span className="relative invisible">Lihat Detail</span>
-              </a>
-            </div>
-          </article>
         </div>
       </div>
     </section>
