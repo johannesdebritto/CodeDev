33eb4f62bc749917b0d022a42c5b1f050ea9e94b
johannes debritto <johanesdebrito22@gmail.com>
2025-05-08 10:30:18 +0700

    push

--- app/HomePage/HeroSection/HeroSection.tsx
+++ app/HomePage/HeroSection/HeroSection.tsx
@@ -7,7 +7,7 @@ const HeroSection = () => {
       <div className="container mx-auto px-6 lg:px-12 grid grid-cols-1 lg:grid-cols-2 gap-12 items-center">
         {/* Kolom Gambar - Mobile muncul duluan */}
         <div className="relative w-full h-[300px] md:h-[400px] lg:h-[500px] overflow-hidden order-1 md:order-2">
-          <Image src="/images/icon/hero.svg" alt="Ilustrasi Maggot BSF Colomadu" fill className="object-contain object-center md:object-right translate-x-0 md:translate-x-2 lg:translate-x-4 animate-fadeIn delay-100" priority />
+          <Image src="/images/cover.svg" alt="Ilustrasi Maggot BSF Colomadu" fill className="object-contain object-center md:object-right translate-x-0 md:translate-x-2 lg:translate-x-4 animate-fadeIn delay-100" priority />
         </div>
         {/*  */}
 
