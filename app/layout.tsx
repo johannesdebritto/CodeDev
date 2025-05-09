1467545d0899341befe29c6d87e321b81cc43f5f
johannes debritto <johanesdebrito22@gmail.com>
2025-05-09 11:30:58 +0700

    push

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,14 @@
-import { Metadata } from "next";
-import "@/app/globals.css";
-import Navbar from "@/app/Components/Navbar";
-import Footer from "@/app/Components/Footer";
-import FloatingButtons from "@/app/Components/FloatingButtons";
-import LoadingWrapper from "@/app/Components/LoadingWrapper"; // Import LoadingWrapper
+import type { Metadata } from "next";
+import "@/app/globals.css"; // Gunakan alias untuk impor global CSS
+import Navbar from "@/app/Components/Navbar"; // Gunakan alias untuk impor Navbar
+import Footer from "@/app/Components/Footer"; // Gunakan alias untuk impor Footer
+import FloatingButtons from "@/app/Components/FloatingButtons"; // Gunakan alias untuk FloatingButtons
 
 export const metadata: Metadata = {
   title: "CodeDev",
   description: "Freelance Website",
   icons: {
-    icon: "/images/logo/logo.ico",
+    icon: "/images/logo/logo.ico", // Path ke favicon
   },
 };
 
@@ -21,18 +20,24 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        {/* Preload Images */}
         <link rel="preload" href="/images/logo/logo.svg" as="image" />
+
         <link rel="preload" href="/images/logo_wa.svg" as="image" />
       </head>
 
       <body className="antialiased">
-        {/* Bungkus konten dengan LoadingWrapper */}
-        <LoadingWrapper>
-          <Navbar />
-          <main>{children}</main>
-          <Footer />
-          <FloatingButtons />
-        </LoadingWrapper>
+        {/* Navbar */}
+        <Navbar />
+
+        {/* Konten Halaman */}
+        <main>{children}</main>
+
+        {/* Footer */}
+        <Footer />
+
+        {/* Floating Buttons */}
+        <FloatingButtons />
       </body>
     </html>
   );
