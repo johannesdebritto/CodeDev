bbbc2d72a9b682c14f6b1441831416db4a9bbd29
johannes debritto <johanesdebrito22@gmail.com>
2025-05-11 19:37:54 +0700

    push

--- app/PortofolioPage/page.tsx
+++ app/PortofolioPage/page.tsx
@@ -42,23 +42,15 @@ const ProjectWeb = () => {
 
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
         {projects.map((project, index) => (
-          <article key={project.id} className={`border rounded-lg shadow-md flex flex-col items-start w-full max-w-xs mx-auto ${index === 3 ? "md:col-start-1" : ""}`} aria-label={`Project ${project.title}`}>
-            <figure className="w-full">
-              <Image src={project.image} alt={`Tampilan ${project.title}`} width={500} height={300} className="w-full h-32 object-cover rounded-t-lg" priority={index === 0} />
-            </figure>
+          <article key={project.id} className="relative group w-full max-w-xs mx-auto overflow-hidden rounded-lg shadow-md">
+            {/* Gambar full */}
+            <Image src={project.image} alt={`Tampilan ${project.title}`} width={400} height={200} className="w-full h-64 object-cover transition-transform duration-300 group-hover:scale-105" priority={index === 0} />
 
-            <div className="px-4 py-6">
-              <h3 className="text-lg font-semibold text-gray-800 mb-2">{project.title}</h3>
-              <span className="px-3 py-1 text-sm bg-blue-100 text-blue-600 rounded-full mb-4">{project.category}</span>
-              <p className="text-gray-600 text-sm mb-3 mt-2">{project.description}</p>
-              <a
-                href="#_"
-                className="relative inline-flex items-center justify-center p-4 px-3 py-1 overflow-hidden font-medium text-indigo-600 transition duration-300 ease-out rounded-full shadow-xl group hover:ring-1 hover:ring-purple-500"
-              >
-                <span className="absolute inset-0 w-full h-full bg-gradient-to-br from-blue-600 via-purple-600 to-pink-700"></span>
-                <span className="absolute bottom-0 right-0 block w-64 h-64 mb-32 mr-4 transition duration-500 origin-bottom-left transform rotate-45 translate-x-24 bg-pink-500 rounded-full opacity-30 group-hover:rotate-90 ease"></span>
-                <span className="relative text-white">Lihat Studi Case</span>
-              </a>
+            {/* Overlay dengan animasi saat hover */}
+            <div className="absolute bottom-0 left-0 w-full h-3/4 bg-black/50 backdrop-blur-md text-white px-4 py-5 transform translate-y-full group-hover:translate-y-0 transition-transform duration-300 ease-in-out">
+              <h3 className="text-lg font-semibold">{project.title}</h3>
+              <span className="inline-block bg-blue-500 text-white text-xs px-2 py-1 rounded-full mt-1">{project.category}</span>
+              <p className="text-sm mt-2">{project.description}</p>
             </div>
           </article>
         ))}
